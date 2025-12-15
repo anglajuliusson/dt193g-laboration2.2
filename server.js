f4c89d97fb14e3f951fcfd3516db6af7128240ea
Ängla Juliusson <angla02@icloud.com>
2025-12-15 10:23:41 +0100

    Lägger till MySQL i Fastify och lägger till route för att testa databasanslutning

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 // Import the framework and instantiate it
 import Fastify from 'fastify'
-const fastify = Fastify({
-  logger: true
-})
+import { excuteQuery } from './config/db.js' // Importera hjälpfunktionen för attt köra SQL-frågor mot MySQL
+
+const fastify = Fastify({ logger: true }); 
 
 // Declare a route
 fastify.get('/', async function handler (request, reply) {
   return { hello: 'world' }
-})
+});
+
+// Route som testar anslutningen till MySQL-databasen
+fastify.get('/dbtest', async () => {
+
+    // Anropar excuteQuery för att skicka SQL till databasen
+    // Resultatet returneras som ett Promise och hanteras med await
+    const result = await excuteQuery("SELECT NOW()");
+
+    // Returnerar resultatet från databasen som JSON
+    return { tid: result };
+  });
 
 // Run the server!
 try {
