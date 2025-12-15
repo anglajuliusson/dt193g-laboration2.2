a1598ae0562520ddaf5477178757b133b8856387
Ängla Juliusson <angla02@icloud.com>
2025-12-15 14:31:04 +0100

    Registrera DELETE-route för conserts/:id

--- routes/consertsRoutes.js
+++ routes/consertsRoutes.js
@@ -1,7 +1,8 @@
-import { getAllConserts, getConsertById } from './conserts.js'; // Importerar controller-funktionerna från conserts.js
+import { deleteConsertById, getAllConserts, getConsertById } from './conserts.js'; // Importerar controller-funktionerna från conserts.js
 
 export async function routes(fastify) {
     // Registrerar GET-route och kopplar till controller-funktionen
     fastify.get('/conserts', getAllConserts); // Route för att hämta alla konserter
     fastify.get('/conserts/:id', getConsertById); // Route för att hämta en konsert med specifikt id
+    fastify.delete('/conserts/:id', deleteConsertById); // Route för att radera en konsert med specifikt id
 };
