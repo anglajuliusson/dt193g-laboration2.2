b7289d3d2969cf9905a67f3c5479df808c6a2787
Ängla Juliusson <angla02@icloud.com>
2025-12-15 12:12:17 +0100

    Registrerar GET-route för conserts/:id

--- routes/consertsRoutes.js
+++ routes/consertsRoutes.js
@@ -1,6 +1,7 @@
-import { getAllConserts } from './conserts.js'; // Importerar controller-funktionerna från conserts.js
+import { getAllConserts, getConsertById } from './conserts.js'; // Importerar controller-funktionerna från conserts.js
 
 export async function routes(fastify) {
     // Registrerar GET-route och kopplar till controller-funktionen
-    fastify.get('/conserts', getAllConserts)
+    fastify.get('/conserts', getAllConserts); // Route för att hämta alla konserter
+    fastify.get('/conserts/:id', getConsertById); // Route för att hämta en konsert med specifikt id
 };
