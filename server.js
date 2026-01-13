2d2bc6370b2ef918659663f50429ca924e82feba
Ängla Juliusson <angla02@icloud.com>
2026-01-13 14:32:56 +0100

    Installera CORS och aktivera det i server.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 // Import the framework and instantiate it
 import Fastify from 'fastify'
 import { routes } from './routes/consertsRoutes.js'; // Importerar route-funktionerna från consertsRoutes.js
+import cors from '@fastify/cors' // Importera cors
 
 const fastify = Fastify({ logger: true }); 
+
+// Aktivera CORS
+await fastify.register(cors, {
+  origin: '*' // Tillåt alla origins (utveckling)
+})
 
 // Registrerar alla routes från routes-filen på Fastify-servern
 // Alla endpoints definierade i consertsRoutes.js blir nu tillgängliga
